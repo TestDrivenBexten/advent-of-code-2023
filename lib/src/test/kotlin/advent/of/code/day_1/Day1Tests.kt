ef6147c2e704419458c2d4d264ebaf5c6c828709
Skyler Bexten <skyler.bexten@gmail.com>
2023-12-02 20:15:43 -0800

    Solve day 1

--- lib/src/test/kotlin/advent/of/code/day_1/Day1Tests.kt
+++ lib/src/test/kotlin/advent/of/code/day_1/Day1Tests.kt
@@ -17,6 +17,16 @@ class Day1Tests {
     }
 
     @Test
+    @DisplayName("Should return 56_397 for big inputs")
+    fun should_return_big_input() {
+        val path = Paths.get("src/test/kotlin/advent/of/code/day_1/Day1Big.txt")
+        val strList = readStringListFromPath(path)
+
+        val sum = strList.map(::parseIntFromString).sum()
+        assertEquals(56_397, sum)
+    }
+
+    @Test
     @DisplayName("Should return 15 for 'a1b3c5de'")
     fun should_parse_number_from_string() {
         val input = "a1b3c5de"
