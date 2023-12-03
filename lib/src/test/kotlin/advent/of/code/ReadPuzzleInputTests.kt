7b87e971720a82cfe7499ba4462d8636ea249b16
Skyler Bexten <skyler.bexten@gmail.com>
2023-12-02 19:31:08 -0800

    Setup Advent of Code project

---
+++ lib/src/test/kotlin/advent/of/code/ReadPuzzleInputTests.kt
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+import kotlin.test.assertEquals
+
+@DisplayName("Puzzle Input Tests")
+class ReadPuzzleInputTests {
+    @Test
+    @DisplayName("Should read 'Hello World' from file")
+    fun should_read_hello_world_for_string_list(){
+        val path = Paths.get("src/test/kotlin/advent/of/code/string.txt")
+        val stringList = readStringListFromPath(path)
+        assertEquals(listOf("Hello","World"), stringList)
+    }
+
+    @Test
+    @DisplayName("Should read integers from file")
+    fun should_read_integer_list(){
+        val path = Paths.get("src/test/kotlin/advent/of/code/integer.txt")
+        val intList = readIntListFromPath(path)
+        assertEquals(listOf(5,17,2359,-34,0), intList)
+    }
+
+    @Test
+    @DisplayName("Should read longs from file")
+    fun should_read_long_list(){
+        val path = Paths.get("src/test/kotlin/advent/of/code/long.txt")
+        val longList = readLongListFromPath(path)
+        assertEquals(listOf(92147483647), longList)
+    }
+}
