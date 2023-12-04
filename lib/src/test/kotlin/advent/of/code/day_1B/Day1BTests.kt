67bc41f8783c9827ef8488f058b26feda539048d
Skyler Bexten <skyler.bexten@gmail.com>
2023-12-03 18:02:56 -0800

    Fix for 1B

--- lib/src/test/kotlin/advent/of/code/day_1B/Day1BTests.kt
+++ lib/src/test/kotlin/advent/of/code/day_1B/Day1BTests.kt
@@ -3,17 +3,27 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import java.nio.file.Paths
 
-@DisplayName("Day 2 Tests")
-class Day2Tests {
+@DisplayName("Day 1B Tests")
+class Day1BTests {
 
     @Test
     @DisplayName("Should return 142 for small inputs")
     fun should_return_small_input() {
-        val path = Paths.get("src/test/kotlin/advent/of/code/day_2/Day2Small.txt")
+        val path = Paths.get("src/test/kotlin/advent/of/code/day_1B/Day1BSmall.txt")
         val strList = readStringListFromPath(path)
 
         val sum = strList.map(::parseSpelledIntFromString).sum()
         assertEquals(281, sum)
+    }
+
+    @Test
+    @DisplayName("Should return 55_701 for big inputs")
+    fun should_return_big_input() {
+        val path = Paths.get("src/test/kotlin/advent/of/code/day_1B/Day1BBig.txt")
+        val strList = readStringListFromPath(path)
+
+        val sum = strList.map(::parseSpelledIntFromString).sum()
+        assertEquals(55_701, sum)
     }
 
     @Test
