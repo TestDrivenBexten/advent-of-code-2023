03058fd00289f01844bc02153a1a42a77342d4c4
Skyler Bexten <skyler.bexten@gmail.com>
2023-12-04 21:20:13 -0800

    Solve day 2 part 1

---
+++ lib/src/main/kotlin/advent/of/code/day_2/Day2.kt
@@ -0,0 +1,68 @@
+enum class Color {
+    RED, GREEN, BLUE
+}
+
+data class Draw(val redCount: Int, val greenCount: Int, val blueCount: Int)
+
+data class Game(val id: Int, val drawList: List<Draw>)
+
+val gameRegex = "Game (\\d+)".toRegex()
+fun parseGame(input: String): Game {
+    // Drop 'Game \d: '
+    val colonIndex = input.indexOf(':')
+    val trimmedInput = input.drop(colonIndex + 2)
+    val rawDrawList = trimmedInput.split(';')
+    val drawList = rawDrawList.map(::parseDraw)
+
+    val matchResult = gameRegex.find(input)
+    val gameId = matchResult?.groups?.get(1)?.value?.toInt() ?: 0
+    return Game(gameId, drawList)
+}
+
+fun parseDraw(input: String): Draw {
+    val rawCountList = input.split(',')
+    val countList = rawCountList.map(::parseCount)
+
+    val redCount = countList.find { it.first == Color.RED }?.second ?: 0
+    val greenCount = countList.find { it.first == Color.GREEN }?.second ?: 0
+    val blueCount = countList.find { it.first == Color.BLUE }?.second ?: 0
+
+    return Draw(redCount, greenCount, blueCount)
+}
+
+fun isPossibleGame(game: Game): Boolean {
+    return game.drawList.all(::isPossibleDraw)
+}
+
+fun isPossibleDraw(draw: Draw): Boolean {
+    val validRedCount = draw.redCount <= 12
+    val validGreenCount = draw.greenCount <= 13
+    val validBlueCount = draw.blueCount <= 14
+    return validRedCount && validGreenCount && validBlueCount
+}
+
+typealias Count = Int
+val numberRegex = "(\\d+)".toRegex()
+fun parseCount(input: String): Pair<Color, Count> {
+    return if (input.contains("green")) {
+        val matchResult = numberRegex.find(input)
+        val green = matchResult?.groups?.get(1)
+        val greenCount = green?.value?.toInt() ?: 0
+        Pair(Color.GREEN, greenCount)
+    } else if (input.contains("blue")) {
+        val matchResult = numberRegex.find(input)
+        val blue = matchResult?.groups?.get(1)
+        val blueCount = blue?.value?.toInt() ?: 0
+        Pair(Color.BLUE, blueCount)
+    } else if (input.contains("red")) {
+        val matchResult = numberRegex.find(input)
+        val red = matchResult?.groups?.get(1)
+        val redCount = red?.value?.toInt() ?: 0
+        Pair(Color.RED, redCount)
+    } else {
+        val matchResult = numberRegex.find(input)
+        val red = matchResult?.groups?.get(1)
+        val redCount = red?.value?.toInt() ?: 0
+        Pair(Color.RED, redCount)
+    }
+}
---
+++ lib/src/test/kotlin/advent/of/code/day_2/Day2Tests.kt
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+
+@DisplayName("Day 2 Tests")
+class Day2Tests {
+    @Test
+    @DisplayName("Should parse game with one draw")
+    fun should_parse_game_with_one_draw() {
+        val input = "Game 3: 8 green, 6 blue, 20 red"
+        val game = parseGame(input)
+
+        val expectedDraw = Draw(20, 8, 6)
+        val expectedGame = Game(3, listOf(expectedDraw))
+        assertEquals(expectedGame, game)
+    }
+
+    @Test
+    @DisplayName("Should parse game with two draws")
+    fun should_parse_game_with_two_draws() {
+        val input = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green"
+        val game = parseGame(input)
+
+        val expectedFirstDraw = Draw(20, 8, 6)
+        val expectedSecondDraw = Draw(4, 13, 5)
+        val expectedGame = Game(3, listOf(expectedFirstDraw, expectedSecondDraw))
+        assertEquals(expectedGame, game)
+    }
+
+    @Test
+    @DisplayName("Should solve for small input")
+    fun should_solve_for_small_input() {
+        val path = Paths.get("src/test/kotlin/advent/of/code/day_2/Day2Small.txt")
+        val strList = readStringListFromPath(path)
+
+        val gameList = strList.map(::parseGame)
+        val possibleGameList = gameList.filter(::isPossibleGame)
+
+        val sum = possibleGameList.map { it.id }.sum()
+        assertEquals(8, sum)
+    }
+
+    @Test
+    @DisplayName("Should solve for big input")
+    fun should_solve_for_big_input() {
+        val path = Paths.get("src/test/kotlin/advent/of/code/day_2/Day2Big.txt")
+        val strList = readStringListFromPath(path)
+
+        val gameList = strList.map(::parseGame)
+        val possibleGameList = gameList.filter(::isPossibleGame)
+
+        val sum = possibleGameList.map { it.id }.sum()
+        assertEquals(2_593, sum)
+    }
+}
